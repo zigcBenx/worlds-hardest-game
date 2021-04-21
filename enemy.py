85d8a27fe2ff3af425eb277abd71578a45368218
Žiga Benko <32687182+zigcBenx@users.noreply.github.com>
2021-04-21 14:11:58 +0200

    Add enemy and player for episode 3

---
+++ enemy.py
@@ -0,0 +1,24 @@
+import pygame
+
+class Enemy:
+    def __init__(self, x, y, width, height, vel=5, color=(0,0,255), right = True):
+        self.x = x
+        self.y = y
+        self.width = width
+        self.height = height
+        self.vel = vel
+        self.color = color
+        self.right = right
+
+    def draw(self, screen):
+        pygame.draw.circle(screen, (0,0,0), (self.x, self.y), self.width+2, self.height+2)
+        return pygame.draw.circle(screen, self.color, (self.x, self.y), self.width, self.height)
+
+
+    def move(self, bound_x1, bound_x2):
+        if self.x < bound_x1 or self.x > bound_x2:
+            self.right = not self.right
+        if self.right:
+            self.x += self.vel
+        else:
+            self.x -= self.vel
