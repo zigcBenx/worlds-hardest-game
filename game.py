c0e1b03353635a5bd382db34fb7b05e2e9a5d523
Žiga Benko <32687182+zigcBenx@users.noreply.github.com>
2021-04-21 14:11:24 +0200

    update game.py for episode3

--- game.py
+++ game.py
@@ -1,5 +1,7 @@
 import pygame
 
+from player import Player
+from enemy import Enemy
 pygame.init()
 
 SCREEN_WIDTH = 800
@@ -13,31 +15,49 @@ screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 isRunning = True
 
 
-x = 50
-y = 50
+player = Player(50, 50, 10, 10)
+enemy = Enemy(50, 100, 5, 5, 15)
+enemy2 = Enemy(50, 150, 5, 5, 15)
+enemy3 = Enemy(250, 150, 5, 5, 15)
+enemy4 = Enemy(250, 200, 5, 5, 15)
 
-vel = 5
+font = pygame.font.Font('freesansbold.ttf', 32)
+
+def update():
+    keys = pygame.key.get_pressed()
+    player.move(keys)
+    enemy.move(50,250)
+    enemy2.move(50,250)
+    enemy3.move(50,250)
+    enemy4.move(50,250)
+    
+    if player.draw(screen).collidelist([enemy.draw(screen), enemy2.draw(screen), enemy3.draw(screen), enemy4.draw(screen)]) != -1:
+        player.reset()
+        player.deaths += 1
+
+def draw():
+    screen.fill((183,175,250))
+    player.draw(screen)
+    enemy.draw(screen)
+    enemy2.draw(screen)
+    enemy3.draw(screen)
+    enemy4.draw(screen)
+
+    deathCounter = font.render("Deaths: " + str(player.deaths), True, (255,255,255))
+    screen.blit(deathCounter, (300,50))
+    
+    pygame.display.update()
 
 while isRunning:
     pygame.time.delay(50)
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             isRunning = False
-    keys = pygame.key.get_pressed()
-    if keys[pygame.K_LEFT]:
-        x -= vel
-    if keys[pygame.K_RIGHT]:
-        x += vel
-    if keys[pygame.K_DOWN]:
-        y += vel
-    if keys[pygame.K_UP]:
-        y -= vel
-
-    screen.fill((0,0,0))
-    pygame.draw.rect(screen, (255,0,0), (x,y,10,10))
+    
+    update()
+    
+    draw()
 
     
-    pygame.display.update()
-
 
 pygame.quit()
